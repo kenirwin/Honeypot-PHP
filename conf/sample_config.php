1814e70786aadd5be547f2b85d4994a5e60e4a7b
Ken Irwin <kirwin@wittenberg.edu>
2018-04-19 09:31:53 -0400

    added sample config

---
+++ conf/sample_config.php
@@ -0,0 +1,9 @@
+<?php
+define ('DB_HOST', '');
+define ('DB_USER', '');
+define ('DB_PASS', '');
+define ('DB_DB', '');
+define ('DB_CHARSET', '');
+define ('CHECK_MINUTES', 3);
+define ('BAN_THRESHOLD', 8);
+
