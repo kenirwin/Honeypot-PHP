046baa6849c894ca5036b532c637fb0eaefd7c70
Ken Irwin <kirwin@wittenberg.edu>
2018-06-19 10:04:59 -0400

    add injection_scan.sh and config

---
+++ conf/sample_config.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+injection_log='' #file_path
+access_log='' #file path
+ip_to_ignore='xxx.xxx' #any part of iprange to ignore. leave as 'xxx.xxx' if none to ignore
---
+++ injection_scan.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+source conf/config.sh
+
+#scan the access log for instances of someone using SQL keywords in their query string; filter out requests from Wittenberg (which will happen when using phpMyAdmin
+# select the IP range with a sed regexp
+# sort and count uniq instances
+# write it to a log
+
+egrep '\bUNION\b|\bSELECT\b|\bCHAR\b' $access_log | grep -v $ip_to_ignore | sed -n 's/\([0-9\.]\+\).*/\1/p' | sort | uniq -c > $injection_log
+
